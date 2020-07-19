b83083fe1e3e4392f7e342937baf921f2c7633e9
wangyue <wynfnone@gmail.com>
2020-07-19 12:08:56 +0800

    all

---
+++ src/main/java/com/ehaohaoxuexi/newgit/NewgitApplication.java
@@ -0,0 +1,13 @@
+package com.ehaohaoxuexi.newgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewgitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ehaohaoxuexi/newgit/NewgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehaohaoxuexi.newgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewgitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
